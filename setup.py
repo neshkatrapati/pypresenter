432a46366ad4ec6b43f0ab2c9eea11b3804ba88c
Ganesh Katrapati <ganesh.katrapati@research.iiit.ac.in>
2014-02-15 22:16:38 +0530

    First Commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pypresenter',
+      version='0.1',
+      description='Presentations from the CommandLine',
+      url='http://github.com/storborg/funniest',
+      author='Ganesh Katrapati',
+      author_email='ganesh@swecha.net',
+      license='GNU GPL V3+',
+      packages=['pypresenter'],
+      zip_safe=False)
