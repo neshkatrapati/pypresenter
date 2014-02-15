bc685c28fb7a993aa83aa55601fed54371d49e19
Ganesh Katrapati <ganesh.katrapati@research.iiit.ac.in>
2014-02-15 22:29:53 +0530

    Added README

--- setup.py
+++ setup.py
@@ -3,9 +3,15 @@ from setuptools import setup
 setup(name='pypresenter',
       version='0.1',
       description='Presentations from the CommandLine',
-      url='http://github.com/storborg/funniest',
+      url='https://github.com/neshkatrapati/pypresenter',
       author='Ganesh Katrapati',
       author_email='ganesh@swecha.net',
       license='GNU GPL V3+',
       packages=['pypresenter'],
+      install_requires = [
+        'pyfiglet',
+        'python-aalib',
+        'colorama',
+        'termcolor',
+      ],
       zip_safe=False)
